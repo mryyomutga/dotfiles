f239d323d12c538973c4222ee012201cd8f4e485
mryyomutga <mryyomutga@gmail.com>
2018-09-27 03:35:11 +0900

    add remove vim script for uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,7 @@ rm -rf ${HOME}/.dotfiles
 rm -rf ${HOME}/.zshrc
 rm -rf ${HOME}/.vimrc
 rm -rf ${HOME}/.config/nvim/init.vim
+rm -rf ${HOME}/.dotfiles/.vim
 rm -rf ${HOME}/.tmux.conf
 rm -rf ${HOME}/.zsh_history
 rm -rf ${HOME}/.config/peco/config.json
